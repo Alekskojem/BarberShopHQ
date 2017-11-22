03dbb5118bb8e08f1d5ed17fc507f49f32259b4d
Aleksandr Kojemyako <alekskojem@gmail.com>
2017-11-22 16:31:57 +0200

    Add barbers

--- db/migrate/20171122142215_create_barbers.rb
+++ db/migrate/20171122142215_create_barbers.rb
@@ -1,4 +1,10 @@
 class CreateBarbers < ActiveRecord::Migration[5.1]
   def change
+  	reate_table :barbers do |t|
+  			t.text :name
   end
+  Barber.create :name => 'Jessie Pinkman'
+  Barber.create :name => 'Walter White'
+  Barber.create :name => 'Gas Fring'
+end
 end
